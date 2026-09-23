71601aa87364d6d58c985189dd7c9d61a39ebd0b
jonny652 <jonny_uk@live.co.uk>
2026-09-23 15:05:08 +0100

    refactor: streamline test structure and improve readability in first-test.cy.ts

--- cypress/e2e/1-getting-started/first-test.cy.ts
+++ cypress/e2e/1-getting-started/first-test.cy.ts
@@ -1,97 +1,60 @@
 /// <reference types="cypress" />
 
 describe("My First Test", () => {
-  it("navigate to the dyson homepage and do stuff", () => {
+  beforeEach(() => {
+    const searchField = '[data-cy="searchFieldSearch"]';
+    const manufacturerTabCategory = '[data-cy="tabCategory"]';
+    const dysonTile = "cirrus-search-result-tile-container a";
+
     cy.visit("https://source.thenbs.com/en/gb/");
-    cy.get('[data-cy="searchFieldSearch"]')
-      .first()
-      .type("Dyson")
-      .type("{enter}");
+    cy.get(searchField).first().type("Dyson{enter}");
 
-    cy.contains('[data-cy="tabCategory"]', "Manufacturers").click();
-    // Page title changed. The page should have `title` value
+    cy.contains(manufacturerTabCategory, "Manufacturers").click();
     cy.title().should("eq", 'Manufacturers matching "Dyson" | NBS Source');
 
-    cy.contains("cirrus-search-result-tile-container a", "Dyson").click();
+    cy.contains(dysonTile, "Dyson").click();
     cy.url().should("include", "/manufacturer/dyson");
   });
-  // check the h1 header paragraph
+
+  //01 check the h1 header paragraph
   it("check the h1 header paragraph", () => {
-        cy.visit("https://source.thenbs.com/en/gb/");
-    cy.get('[data-cy="searchFieldSearch"]')
-      .first()
-      .type("Dyson")
-      .type("{enter}");
-
-    cy.contains('[data-cy="tabCategory"]', "Manufacturers").click();
-    // Page title changed. The page should have `title` value
-    cy.title().should("eq", 'Manufacturers matching "Dyson" | NBS Source');
-
-    cy.contains("cirrus-search-result-tile-container a", "Dyson").click();
-    cy.url().should("include", "/manufacturer/dyson");
-    cy.get(".brand-title-container + p")
-      .should("be.visible")
-      .and("have.text", "Technology for business");
+    const h1Header = "h1";
+    cy.get(h1Header).should("be.visible").and("have.text", "Dyson");
   });
 
- // check the dyson telephone number 
+  //02 check the dyson telephone number
   it("check the dyson telephone number", () => {
-        cy.visit("https://source.thenbs.com/en/gb/");
-            cy.get('[data-cy="searchFieldSearch"]')
-              .first()
-              .type("Dyson")
-              .type("{enter}");
-        
-            cy.contains('[data-cy="tabCategory"]', "Manufacturers").click();
-            // Page title changed. The page should have `title` value
-            cy.title().should("eq", 'Manufacturers matching "Dyson" | NBS Source');
-        
-            cy.contains("cirrus-search-result-tile-container a", "Dyson").click();
-            cy.url().should("include", "/manufacturer/dyson");
-          
-        cy.get('a[action="telephone"]').should('have.text', ' 08003457788 ');
-        cy.get('a[action="telephone"]').should('have.attr', 'title', 'Call 08003457788');
-        cy.get('a[action="telephone"]').should('have.attr', 'href', 'tel:08003457788');
+    const dysonTelephone = 'a[action="telephone"]';
+    cy.get(dysonTelephone).should("be.visible");
+
+    cy.get(dysonTelephone).should("have.text", " 08003457788 ");
+    cy.get(dysonTelephone).should("have.attr", "title", "Call 08003457788");
+    cy.get(dysonTelephone).should("have.attr", "href", "tel:08003457788");
   });
 
- // check the dyson website link
+  //03 check the dyson website link
   it("check the dyson website link", () => {
-        cy.visit("https://source.thenbs.com/en/gb/");
-            cy.get('[data-cy="searchFieldSearch"]')
-              .first()
-              .type("Dyson")
-              .type("{enter}");
-        
-            cy.contains('[data-cy="tabCategory"]', "Manufacturers").click();
-            // Page title changed. The page should have `title` value
-            cy.title().should("eq", 'Manufacturers matching "Dyson" | NBS Source');
-        
-            cy.contains("cirrus-search-result-tile-container a", "Dyson").click();
-            cy.url().should("include", "/manufacturer/dyson");
-          
-        cy.get('a[action="company-website"]').should('have.text', ' Website ');
-        cy.get('a[action="company-website"]').should('have.attr', 'href', 'https://www.dyson.co.uk/commercial/overview');
-        cy.get('a[action="company-website"]').should('have.attr', 'target', '_blank');
-        cy.get('a[action="company-website"]').should('be.visible');
-        cy.get('a[action="company-website"]').should('have.attr', 'title', 'Visit https://www.dyson.co.uk/commercial/overview');
+    const dysonWebsite = 'a[action="company-website"]';
+    cy.get(dysonWebsite).should("have.text", " Website ");
+    cy.get(dysonWebsite).should(
+      "have.attr",
+      "href",
+      "https://www.dyson.co.uk/commercial/overview",
+    );
+    cy.get(dysonWebsite).should("have.attr", "target", "_blank");
+    cy.get(dysonWebsite).should("be.visible");
+    cy.get(dysonWebsite).should(
+      "have.attr",
+      "title",
+      "Visit https://www.dyson.co.uk/commercial/overview",
+    );
   });
 
-  // check the contact manfacurer link
+  //04 check the contact manfacurer link
   it("check the contact manfacurer link", () => {
-        cy.visit("https://source.thenbs.com/en/gb/");
-            cy.get('[data-cy="searchFieldSearch"]')
-              .first()
-              .type("Dyson")
-              .type("{enter}");
-        
-            cy.contains('[data-cy="tabCategory"]', "Manufacturers").click();
-            // Page title changed. The page should have `title` value
-            cy.title().should("eq", 'Manufacturers matching "Dyson" | NBS Source');
-        
-            cy.contains("cirrus-search-result-tile-container a", "Dyson").click();
-            cy.url().should("include", "/manufacturer/dyson");
-        cy.get('button.contact-button span.mdc-button__label').should('have.text', ' Contact manufacturer ');
-        cy.get('button.contact-button span.mdc-button__label').should('be.visible');
-  });
+    const contactManufacturer = "button.contact-button span.mdc-button__label";
 
+    cy.get(contactManufacturer).should("have.text", " Contact manufacturer ");
+    cy.get(contactManufacturer).should("be.visible");
+  });
 });
